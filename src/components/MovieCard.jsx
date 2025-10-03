3fd5e4ac3787861cd406c57ee5e33e017c78ae37
Shivam Salkar <salkarshivam28@gmail.com>
2025-10-03 15:16:25 +0530

    Added movie overview

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,11 @@ const MovieCard = ({movie}) => {
 
       {/* <p key={movie.id} className='text-white'>{movie.title}</p> */}
       <div className='flex flex-col '>
-        <p key={movie.id} className='text-white text-[16px] font-bold mt-2'>{movie.title}</p>
+        <p key={movie.id} className='text-white text-[16px] font-bold mt-2 mb-2'>{movie.title}</p>
+
+        <div>
+          <p className='text-[12px] text-gray-500'>{ movie.overview ?  movie.overview.split(' ').slice(0, 25).join(" "):"N/A"}. . . </p>
+        </div>
 
         <div className='flex flex-row mt-2'>
           <img src="https://img.icons8.com/stickers/100/star--v1.png" alt="" className='w-4 h-4' />       
